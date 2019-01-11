216ee3d787a6cd26ab3487ff3875f6ad67586928
Aneisha-m <AMallikaratchy@spartaglobal.com>
2019-01-11 11:44:04 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// alert ("hello");
+
+console.log("hi");
+var myNumber = 90;
+console.log(myNumber);
+
+var number1 = 20;
+var number2 = 30;
+console.log(number2-number1);
+console.log(number1*number2);
+console.log(number1+number2);
+
+var name = "cat";
+var names = "black";
+console.log(names+name);
+
+console.log(number1 +name);
